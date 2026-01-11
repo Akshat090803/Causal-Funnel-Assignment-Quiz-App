dbbbe81803ca0cd30bc63e86c28ad4126d8a9fd4
Akshat090803 <akshatjain70233@gmail.com>
2026-01-11 18:34:37 +0530

    fix height issue

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <QuizContextProvider>
-      <div className="bg-black min-h-screen flex flex-col">
+      <div className="bg-black min-h-svh md:min-h-screen flex flex-col">
         <Header />
         <div className="container mx-auto p-10 pt-20 pb-5  flex-1 flex justify-center">
           <Outlet />
